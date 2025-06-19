c4f565434062685c786c6450336c44116ee8d711
dungle2107 <ttd21072004@gmail.com>
2025-06-20 00:24:09 +0700

    update serviceUrl of getParkingLotDetail

--- src/app/core/constants/endpoints/map-endpoints.ts
+++ src/app/core/constants/endpoints/map-endpoints.ts
@@ -5,5 +5,5 @@ export const MAP_ENDPOINTS = {
   getRoute: (userCoords: number[], pinCoords: number[]) =>
     `https://api.mapbox.com/directions/v5/mapbox/driving/${userCoords[0]},${userCoords[1]};${pinCoords[0]},${pinCoords[1]}?geometries=geojson&access_token=${ENVIRONMENT.mapboxAccessToken}`,
   getParkingLotLocation: (param: string) => ENVIRONMENT.serviceUrl.default + `/ParkingLot/search/${param}`,
-  getParkingLotDetail: (id: string) => `/detail/${id}`
+  getParkingLotDetail: (id: string) => `/ParkingLot/detail/${id}`
 }
