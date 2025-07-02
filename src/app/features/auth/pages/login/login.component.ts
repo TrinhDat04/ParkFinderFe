2bd0572cbfc3a18493a151907df1118aeb8b3d2b
boyfinger <122414066+boyfinger@users.noreply.github.com>
2025-07-02 22:05:51 +0700

    stricter checks & refine ui for login/register

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -23,6 +23,10 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    const token = localStorage.getItem('user_token');
+    if (token) {
+      this.router.navigate(['/homepage']); // redirect wherever makes sense
+    }
   }
 
   login() {
@@ -60,6 +64,44 @@ export class LoginComponent implements OnInit {
           console.error('Error:', error);
         }
       });
+  }
+
+  async loginWithGoogle() {
+    const token = await this.authService.getFirebaseToken();
+    if (token) {
+      this.authService.loginWithGG({token})
+        .subscribe({
+          next: response => {
+            this.message = "Đăng nhập thành công";
+            localStorage.setItem("user_token", response.token);
+            const tokenParts = response.token.split('.');
+            if (tokenParts.length === 3) {
+              const payload = JSON.parse(atob(tokenParts[1]));
+              const userId = payload.Id;
+              localStorage.setItem("user_id", userId);
+            } else {
+              console.warn('Invalid JWT token format');
+            }
+            console.log('Login successful');
+            Swal.fire({
+              icon: 'success',
+              title: 'Đăng nhập thành công!',
+              text: 'Chào mừng bạn quay trở lại!',
+              timer: 2000,
+              showConfirmButton: false
+            }).then(() => {
+              this.router.navigate(['/homepage'])
+            });
+          },
+          error: error => {
+            this.message = 'Đăng nhập thất bại, vui lòng thử lại sau';
+            console.error('Error: ', error)
+          }
+        });
+    } else {
+      this.message = 'Đăng nhập thất bại, vui lòng thử lại sau';
+      console.log("Failed to receive token");
+    }
   }
 
 }
